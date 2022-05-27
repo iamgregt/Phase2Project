4a1516f7dfe1fc8d56cb8f18cbe7fb90e43c227d
GregT <taylor.gregory2@gmail.com>
2022-05-26 20:01:14 -0600

    home css added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   <Router>
   <Navbar />
   <Switch>
-    <Route path="/" exact element={<Home />} />
+    <Route path="/" exact component={Home} />
   </Switch>
   </Router>
   </>  
