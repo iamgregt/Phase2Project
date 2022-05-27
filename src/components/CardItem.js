99deda33ea1e62ced16f8c1f018fefb170cb3621
GregT <taylor.gregory2@gmail.com>
2022-05-26 21:14:30 -0600

    props added to cards

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-function CardItem() {
+function CardItem(props) {
   return (
     <>
       <li className='cards__item'>
-        <Link className='cards__items__link'>
-           <figure className='cards__item__pic-wrap'>
+        <Link className='cards__items__link' to={props.path}>
+           <figure className='cards__item__pic-wrap' data-category={props.label}>
                <img src='/' alt='Travel Image' className='cards__item__img'/>
            </figure>
            <div className='cards__item__info'>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CardItem from './CardItem'
+import './Cards.css'
 
 function Cards() {
   return (
@@ -8,12 +9,16 @@ function Cards() {
       <div className='cards__container'>
           <div className='cards__wrapper'>
               <ul className='cards__items'>
-                  <CardItem />
+                  <CardItem
+                  src='images/img-9.jpg'
+                  text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                  label='adventure'
+                  path='/services' 
+                  />
               </ul>
           </div>
       </div>
     </div>
   )
 }
- className='cards'
 export default Cards
