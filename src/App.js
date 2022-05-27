01740208039e7409cacf0c6bef54d4b9b4d3f37e
GregT <taylor.gregory2@gmail.com>
2022-05-26 22:14:03 -0600

    rough draft complete

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/pages/Home.js'
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
     <Route path="/products" exact component={Products} />
     <Route path="/sign-up" exact component={SignUp} />
   </Switch>
+    <Footer />
   </Router>
   </>  
   );
