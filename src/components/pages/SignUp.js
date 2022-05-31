065b81f572d2cd461b927751d078a76bb718d733
GregT <taylor.gregory2@gmail.com>
2022-05-30 23:05:08 -0600

    POST working

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -7,11 +7,19 @@ export default function SignUp() {
     function handleSubmit(e){
         e.preventDefault()
         console.log(e.target[0].value)
-        fetch('http://localhost:3000/posts')
-        .then(resp => resp.json())
-        .then(data => {
-            console.log(data)
+        fetch('http://localhost:3000/comments', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                firstName: e.target[0].value,
+                lastName: e.target[1].value,
+                email: e.target[2].value
+
+            })
         })
+        .then(resp =>console.log(resp))
         
     }
 
@@ -23,8 +31,16 @@ export default function SignUp() {
             </div>
             <div className='sign-up-form'>
         <form onSubmit={handleSubmit}>
+            <label>First Name
+            <input type='text' placeholder='Your First Name'/>
+            </label>
+            <label>Email
+            <input type='text' placeholder='Your Last Name'/>
+            </label>
+            <label>Email
             <input type='email' placeholder='Enter Your Email Here'/>
             <input type='submit' value='Submit'/>
+            </label>
         </form>
             </div>
         </div>
