8631e5085548fb0fd2cefe62882eed9fac186caa
GregT <taylor.gregory2@gmail.com>
2022-05-30 22:16:15 -0600

    Form updated

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -2,14 +2,29 @@ import React from 'react'
 import '../../App.css'
 
 export default function SignUp() {
+    
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(e.target[0].value)
+        fetch('http://localhost:3000/posts')
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+        })
+        
+    }
+
+
     return (
         <div className='sign-up'>
             <div>
                 <h2>Sign Up</h2>
             </div>
             <div className='sign-up-form'>
-        <form>
-            <input type='email' placeholder='Email'/>
+        <form onSubmit={handleSubmit}>
+            <input type='email' placeholder='Enter Your Email Here'/>
+            <input type='submit' value='Submit'/>
         </form>
             </div>
         </div>
